d0b2751245452ba526e5b98f6a68997dc41f230f
SpaceIbrahim <ibrahimm4566@gmail.com>
2023-10-13 17:06:02 -0400

    Got it to work W

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
+
+options = Options()
+options.add_experimental_option('detach', True)
+
+driver = webdriver.Chrome(service=Service("chrome webdriver/chromedriver"), options=options)
+
+driver.get("https://www.google.com")
